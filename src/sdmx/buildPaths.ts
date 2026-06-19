1d055a49eac759c66cd8e59178ee86574ad18633
Mikhail Hahalushka <Mikhail_Hahalushka@epam.com>
2026-06-19 15:07:17 +0400

    refactor: extract chart threshold and SDMX path constants

--- src/sdmx/buildPaths.ts
+++ src/sdmx/buildPaths.ts
@@ -1,7 +1,8 @@
 import type { SdmxQuery } from "../bridge/types";
+import { SDMX_BASE_PATH, SDMX_DEFAULT_ATTRIBUTES } from "../constants/sdmx";
 
 export function dataPath(q: SdmxQuery["sdmx"]): string {
-  const base = `/sdmx/3.0/data/dataflow/${q.agency_id}/${q.resource_id}/${q.version}/${q.key}`;
-  const params = new URLSearchParams({ attributes: "all", ...(q.params ?? {}) });
+  const base = `${SDMX_BASE_PATH}/${q.agency_id}/${q.resource_id}/${q.version}/${q.key}`;
+  const params = new URLSearchParams({ attributes: SDMX_DEFAULT_ATTRIBUTES, ...(q.params ?? {}) });
   return `${base}?${params}`;
 }
